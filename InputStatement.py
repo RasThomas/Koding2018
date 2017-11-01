54d76a3b703568976ab1e10135b0021bdc09fd2b
GrimPagan <RasThomas@gmail.com>
2017-11-01 20:45:58 +0100

    No love for negative pie

--- InputStatement.py
+++ InputStatement.py
@@ -7,13 +7,18 @@ print("Please enter the radis of the circle:")
 
 radius = float(input())
 
-print("You have entered a radius of ", radius)
+if(radius < 0):
+    print("You have typed a negative value")
+    exit()
+else:
 
-print("The radius of the circle is ", radius)
+    print("You have entered a radius of ", radius)
 
-print("The areal of the circle is ", myConstPi *radius*radius)
+    print("The radius of the circle is ", radius)
 
-print("The conference of the circle is ", 2 *myConstPi *radius)
+    print("The areal of the circle is ", myConstPi *radius*radius)
+
+    print("The conference of the circle is ", 2 *myConstPi *radius)
 #Casting
-print("The conference of the circle as an interger is ", int(2 *myConstPi *radius))
+    print("The conference of the circle as an interger is ", int(2 *myConstPi *radius))
 

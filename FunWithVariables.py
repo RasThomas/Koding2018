4eaa03eb0bd75b42eee22f5cb21e3b50b14d2c73
GrimPagan <RasThomas@gmail.com>
2017-11-01 20:17:38 +0100

    For the love of Pi

---
+++ FunWithVariables.py
@@ -0,0 +1,15 @@
+
+#This is a variable
+radius= 5
+
+#This is a constant
+myConstPi = 3.14
+
+print("The radius of the circle is ", radius)
+
+print("The areal of the circle is ", myConstPi *radius*radius)
+
+print("The conference of the circle is ", 2 *myConstPi *radius)
+#Casting
+print("The conference of the circle as an interger is ", int(2 *myConstPi *radius))
+s

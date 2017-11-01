4dcccdbaf83e273b45b2e3862fd3047b124a9a7e
GrimPagan <RasThomas@gmail.com>
2017-11-01 20:02:43 +0100

    More Print

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,5 @@
 #Test
-test=1
+test=0
 test2="1"
 print("Hello World!!!")
 
@@ -9,7 +9,11 @@ print(10+5)
 
 print("10+5")
 
+print(10 / 2)
+
+print(10 / 3)
+
 while test < 10:
     print("This is test number " +str(test2))
     test+=1
-    test2 = str(test)
+    test2 = str(test+1)

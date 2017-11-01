e6a5a0db9f2966f95c232f5ffbc0957eb001a4c6
GrimPagan <RasThomas@gmail.com>
2017-11-01 20:34:16 +0100

    For the love of Pi 2

---
+++ InputStatement.py
@@ -0,0 +1,19 @@
+myConstPi = 3.14
+radius= 0
+
+#Here we want to take an input form the user
+
+print("Please enter the radis of the circle:")
+
+radius = float(input())
+
+print("You have entered a radius of ", radius)
+
+print("The radius of the circle is ", radius)
+
+print("The areal of the circle is ", myConstPi *radius*radius)
+
+print("The conference of the circle is ", 2 *myConstPi *radius)
+#Casting
+print("The conference of the circle as an interger is ", int(2 *myConstPi *radius))
+

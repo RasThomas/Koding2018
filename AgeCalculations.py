777efcf5ead771d3c10b3c82b0d55cadaf0f7c13
RasThomas <RasThomas@gmail.com>
2017-11-28 17:49:11 +0100

    Now with redo if wrong

--- AgeCalculations.py
+++ AgeCalculations.py
@@ -1,34 +1,44 @@
 import datetime
 
-birthYear = int(input("Please enter year you were born "))
+
 
 currentYear=int(datetime.date.today().strftime("%Y"))
-
+correctBirth = False
 #print("We are in the year ", currentYear)
+while(correctBirth==False):
+    birthYear = int(input("Please enter year you were born "))
+    if(birthYear < 1900):
+      print("That's too old!")
+    elif(birthYear >= currentYear):
+       print("That year is in the future")
+    else:
+        print("You were born in",birthYear)
+        correctBirth=True
+        break
 
-if(birthYear < 1900):
-    print("That's too old!")
-    exit()
-elif(birthYear >= currentYear):
-    print("That year is in the future")
-    exit()
+futureYear = currentYear
 
-futureYear = int(input("Please enter a year in the future "))
+correctFuture = False
+while(correctFuture == False):
+    futureYear = int(input("Please enter a year in the future "))
+    if(futureYear < currentYear):
+        print("You did not type in a year in the future!")
 
-if(futureYear < currentYear):
-    print("You did not type in a year in the future!")
-    exit()
-elif(futureYear > currentYear + 120):
-    print("You will probably not live beyond 120 years.")
-    exit()
+    elif(futureYear > birthYear + 120):
+        print("You will probably not live beyond 120 years.")
+
+    else:
+        print(futureYear, "is in the future!")
+        correctYear = True
+        break
 
 age = currentYear - birthYear
 
-print("You are ", age, " in this year. ")
+print("You are ",age," year old in the year",currentYear)
 
 futureAge = futureYear - birthYear
 
-print("Your age in ", futureYear, " will be: ", futureAge)
+print("Your age in ",futureYear," will be: ", futureAge)
 
 if(age == 100):
     print("Congratulations on being 100 year old.")
@@ -40,23 +50,23 @@ if(age > 14 and age < 19):
 ageLength = len(str(age))
 age_str = str(age)
 ageLast = int(age_str[ageLength -1])
-print("Last number in age ", ageLast)
+#print("Last number in age ", ageLast)
 
 if(ageLast == 0):
-    print("You are ", age, "old and have started a new decade.")
+    print("You are", age, "old and have started a new decade.")
 
 birth_str = str(birthYear)
 yearLength = len(birth_str)
 year_str = str(yearLength)
 yearNumber = int(year_str)
 yearTotal = 0
-print("Number of digits", yearNumber, " year ", birth_str)
+#print("Number of digits", yearNumber, " year ", birth_str)
 
 while(yearNumber > 0):
-    print(birth_str[yearNumber-1])
+   # print(birth_str[yearNumber-1])
     numberYear = int(birth_str[yearNumber-1])
     yearTotal = yearTotal + numberYear
     yearNumber = yearNumber -1
-    print(yearTotal)
+    #print(yearTotal)
 
-print("The total sum of years of the birthyear is ",yearTotal,".")
+print("The total sum of years of the birthyear is",yearTotal,".")

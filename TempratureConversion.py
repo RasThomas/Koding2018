ab9b606c446f5e5813d4c0ea836ddc6dbc502cbf
GrimPagan <RasThomas@gmail.com>
2017-11-29 20:28:07 +0100

    Temperature conversion and fuctions part 2

--- TempratureConversion.py
+++ TempratureConversion.py
@@ -1,29 +1,53 @@
 
 
-fahrInput = int(input("Type in temrature in Fahrenheit for conversion to Celsius. "))
+# fahrInput = int(input("Type in temrature in Fahrenheit for conversion to Celsius. "))
+#
+# celInput = int(input("Type in temprature in Celsius for conversion to Fahrenheit. "))
+#
+# celConv = (((fahrInput - 32)/9) * 5)
+#
+# fahrConv = (((celInput * 9) + 160)/5)
+#
+# print(fahrInput, " in Fahrenheit is ",celConv, " in Celsius")
+#
+# print(celInput, " in Celsius is ",fahrConv, " in Fahrhenheit")
 
-celInput = int(input("Type in temprature in Celsius for conversion to Fahrenheit. "))
 
-celConv = (((fahrInput - 32)/9) * 5)
 
-fahrConv = (((celInput * 9) + 160)/5)
 
-print(fahrInput, " in Fahrenheit is ",celConv, " in Celsius")
-
-print(celInput, " in Celsius is ",fahrConv, " in Fahrhenheit")
 
 #Defining a function
 def celciusToFahrenheit():
     myInput = int(input("Enter a temperature in Celsius :"))
-    tempInFahrenheit = (((celInput * 9) + 160)/5)
+    tempInFahrenheit = (((myInput * 9) + 160)/5)
     print("The temperature of ",myInput, "in celsius is ",tempInFahrenheit," in Fahrenheit")
 
 def fahrenheitToCelsius():
     myInput = int(input("Enter a temperature in Fahrenheit :"))
-    tempInCelsius = (((fahrInput - 32)/9) * 5)
+    tempInCelsius = (((myInput - 32)/9) * 5)
     print("The temperature of ", myInput, "in fahrenheit is ", tempInCelsius," in Celsius")
+
+def isInt(test):
+    isinstance(test, int)
 
 #Calling a function
 
-celciusToFahrenheit()
-fahrenheitToCelsius()
+#celciusToFahrenheit()
+#fahrenheitToCelsius()
+
+
+
+tempTrue = False
+
+while(tempTrue == False):
+    tempType = input("Type F for convertion to Fahrenheit and C for convertion to Celsius: ")
+    if(tempType == "F" or  tempType == "f"):
+        celciusToFahrenheit()
+        tempTrue=True
+    elif(tempType == "C" or  tempType == "c"):
+        fahrenheitToCelsius()
+        tempTrue=True
+    else:
+        print("You did not type in correct letter!")
+
+

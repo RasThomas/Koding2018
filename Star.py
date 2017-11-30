ad3c2ea794fe40e91c8fd8cc32ded35b02cb3e55
RasThomas <RasThomas@gmail.com>
2017-11-30 16:03:49 +0100

    Star fixed with correct degrees

--- Star.py
+++ Star.py
@@ -1,11 +1,13 @@
 import turtle
 
+size = input("How large do you want the sides of the star to be? ")
+
 myTurtle = turtle.Turtle()
 
 turns = 5
 while(turns > 0 ):
-    myTurtle.right(145)
-    myTurtle.forward(50)
+    myTurtle.right(144)
+    myTurtle.forward(float(size))
     turns = turns -1
 
 

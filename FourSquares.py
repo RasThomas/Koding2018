0128c3baa032a948ecc22de26c077b8bea203812
RasThomas <RasThomas@gmail.com>
2017-11-30 15:52:35 +0100

    4 squares inside eachother

---
+++ FourSquares.py
@@ -0,0 +1,43 @@
+import turtle
+import math
+
+myTurtle = turtle.Turtle()
+
+size = int(input("How big do you want the sides of the inital square to be?" ))
+
+distance = int(input("What distance do you want between the squares? "))
+
+def square(size):
+    print("Square")
+    sqaureSides = 4
+    while(sqaureSides > 0):
+        myTurtle.forward(size)
+        myTurtle.right(90)
+        sqaureSides = sqaureSides - 1
+
+def hypotenus(distance):
+    print("Hyp")
+    hyp = math.sqrt(distance*distance*2)
+    print(hyp)
+    return hyp
+
+def invinsibleMove(hyp):
+    print("Move")
+    myTurtle.penup()
+    myTurtle.right(45)
+    myTurtle.forward(hyp)
+    myTurtle.left(45)
+    myTurtle.pendown()
+
+squareNumber = 4
+length = size
+hyp = hypotenus(distance)
+while(squareNumber > 0):
+    sides = 4
+
+    square(length)
+    squareNumber = squareNumber -1
+    length = length - 2 * distance
+
+    invinsibleMove(hyp)
+turtle.done()

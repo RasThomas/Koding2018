16654dfb0e149a8be3319c3d9f64041f6b04432a
RasThomas <RasThomas@gmail.com>
2018-01-07 15:53:35 +0100

    Made different uses for List

---
+++ Day5/Homework/ListWork.py
@@ -0,0 +1,38 @@
+
+
+testList = [1,2,3,5,6,4]
+#pick out largest number
+largestNumber = testList[0]
+
+for i in range(0, len(testList)):
+    if(testList[i]>largestNumber):
+        largestNumber = testList[i]
+
+print(largestNumber)
+#sum of the numbers in the list
+totalList = 0
+
+for i in range(0, len(testList)):
+    totalList = totalList + testList[i]
+
+print(totalList)
+
+#multiply every number in the list
+multiList = 1
+
+for i in range(0, len(testList)):
+    multiList = multiList * testList[i]
+
+print(multiList)
+
+
+
+thirdList = []
+#printing every third item in list
+print(len(thirdList))
+
+for i in range(0, len(testList)):
+    if((i+2) % 3 == 1):
+        thirdList.append(testList[i])
+
+print(thirdList)

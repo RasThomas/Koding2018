325aecd4fe4fe082f0f012c60f503f3ef23a7333
RasThomas <RasThomas@gmail.com>
2018-01-16 17:23:53 +0100

    Reading and creating files, passwordcheck

---
+++ Day6/Classwork/FileOperations.py
@@ -0,0 +1,58 @@
+#reading a file
+
+def readMyDataFile(dateFile):
+    with open(dateFile, mode ='r') as myDataFile:
+        item = myDataFile.read().splitlines()
+        print(type(item))
+        print(item)
+        return item
+
+
+readMyDataFile("someData.txt")
+
+def writeMyDataFile(moreClubs):
+    with open("moreData.txt", mode='w') as myDataFile:
+        for club in moreClubs:
+            myDataFile.write(club+'\n')
+        print (moreClubs)
+
+moreClubs =["Ajax", "Bremen"]
+
+writeMyDataFile(moreClubs)
+
+def seeIfItemExists(myItem):
+    items = readMyDataFile("someData.txt")
+    for item in items:
+        if(item == myItem):
+            print(myItem, " in the list already.")
+            return
+
+    print(myItem, " was not in the list.")
+    items.append(myItem)
+    writeMyDataFile(items)
+    print("Made new list with ", myItem)
+
+seeIfItemExists("Liverpool")
+
+seeIfItemExists("Ajax")
+
+
+#Fix this
+def checkPassword(userName):
+    data = readMyDataFile("UserCredentials.txt")
+    print(data)
+    for user in data:
+        userCred = user.split(';')
+        print(userCred[0])
+        if(userCred[0] == userName):
+            print("Type password for", userName)
+            userPass = input()
+            if(userPass == userCred[1]):
+                print("Password is correct")
+                break
+            else:
+                print("Password is inccorect")
+
+    print("tests")
+
+checkPassword("Dinesh")
---
+++ Day6/Classwork/Rehash.py
@@ -0,0 +1,29 @@
+myNumber = int(input("Enter a number"))
+
+sumOfDigits = 0
+
+while myNumber > 0:
+    sumOfDigits = sumOfDigits + myNumber % 10
+    myNumber = int (myNumber /10)
+    print("sumOfDigits =", sumOfDigits, " myNumber = ", myNumber)
+
+print("Sum of Didigts is", sumOfDigits)
+
+myList = [1,2,["a","b","c"],5.0,-6.0]
+
+def listing(testList):
+    for i in range(0, len(testList)):
+        if(isinstance(testList[i],list)):
+            listing(testList[i])
+        else:
+            print(testList[i],type(testList[i]))
+
+listing(myList)
+
+def arealRectangle(length, width):
+    areal = length * width
+    return areal
+
+size = arealRectangle(3,2)
+
+print(size)

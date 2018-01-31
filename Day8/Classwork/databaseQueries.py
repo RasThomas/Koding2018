79454255c497ab42d157c8a1cdc18e1efb554c47
303PC09 <RasThomas@gmail.com>
2018-01-31 20:57:06 +0100

    Made stuff otu of Cherrypy, also some more database coding

--- Day8/Classwork/databaseQueries.py
+++ Day8/Classwork/databaseQueries.py
@@ -1,1 +1,38 @@
 
+import sqlite3
+
+def insertIntoTable(createSQL):
+    with sqlite3.connect("expressoHouse.db") as myDatabase:
+        cursor = myDatabase.cursor()
+        cursor.execute(createSQL)
+        myDatabase.commit()
+        pass
+
+#insertSql = """INSERT INTO customer VALUES ("NULL, "Drew Barrymore", "5006, Bergen", 238923892, "drew.arrymore", "123456",1,1)"""
+
+insertSql = """INSERT INTO customer (Name, Adress, Phone, Username, Password, PaymentTypeid, Membershipid) VALUES ("Drew Barrymore", "5006, Bergen", 238923892, "drew.barrymore", "123456",1,1)"""
+
+#insertIntoTable(insertSql)
+
+def instertIntoCustomerTable(item):
+    customer = """INSERT INTO customer (Name, Adress, Phone, Username, Password, PaymentTypeid, Membershipid) VALUES ("""+item+""")"""
+    print(customer)
+    insertIntoTable(customer)
+    pass
+
+instertIntoCustomerTable('"Drew Barrymore","5006, Bergen", 453135435,"drew.Barrymore", "123456", 1, 2')
+
+def getDataFromTable(createSQL):
+    with sqlite3.connect("ExpressoHouse.db") as myDatabase:
+        cursor = myDatabase.cursor()
+        cursor.execute(createSQL)
+        myDatabase.commit()
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+        pass
+
+getSQL = """SELECT * FROM customer"""
+
+getDataFromTable(getSQL)

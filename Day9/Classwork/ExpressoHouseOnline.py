724aef41f8217ac315bb68397875a8ad0b33542b
RasThomas <RasThomas@gmail.com>
2018-02-04 20:11:07 +0100

    Added bit on the functions

--- Day9/Classwork/ExpressoHouseOnline.py
+++ Day9/Classwork/ExpressoHouseOnline.py
@@ -40,13 +40,27 @@ class ExpressoHouse():
                   <body>
                     <div>Welcome to Expresso House</div>
                     <br /> 
-                    <form method="get" action="addCustomerForm">
-                      <button type="submit">Adding new customer!</button>
+                    <form method="get" action="addOrder">
+                      <button type="submit">Adding new order!</button>
                     </form>
                     <br /> 
                     <form method="get" action="showCustomers">
                       <button type="submit">Show customers!</button>
                     </form>
+                    <br /> 
+                    <form method="get" action="addCustomerForm">
+                      <button type="submit">Adding new customer!</button>
+                    </form>
+                    <br /> 
+                    <form method="get" action="searchCustomers">
+                      <button type="submit">Search customers!</button>
+                    </form>
+                    <br /> 
+                    <form method="get" action="products">
+                      <button type="submit">Products!</button>
+                    </form>
+                    <br /> 
+                    <br /> 
                   </body>
                 </html>"""
 
@@ -70,13 +84,13 @@ class ExpressoHouse():
                     <div>Welcome to Expresso House</div>
                     <br /> 
                     <form method="get" action="addCustomer">
-                        <input type="text" value="Customer Name" name="Name" /><br/>
-                        <input type="text" value="Customer Adress" name="Adress" /><br/>
-                        <input type="text" value="Customer Phone" name="Phone" /><br/>
-                        <input type="text" value="Customer Username" name="Username" /><br/>
-                        <input type="text" value="Customer Password" name="Password" /><br/>
-                        <select name="PaymentType">"""+payment+"""</select><br/>   
-                        <select name="MembershipType">"""+membership+"""</select><br/>  
+                        <input type="text" value="Customer Name" name="personName" /><br/>
+                        <input type="text" value="Customer Adress" name="adress" /><br/>
+                        <input type="text" value="Customer Phone" name="phone" /><br/>
+                        <input type="text" value="Customer Username" name="username" /><br/>
+                        <input type="text" value="Customer Password" name="password" /><br/>
+                        <select name="paymentTypeid">"""+payment+"""</select><br/>   
+                        <select name="membershipid">"""+membership+"""</select><br/>  
                       <button type="submit">Adding new customer!</button>
                     </form>
                     <br /> 
@@ -84,11 +98,46 @@ class ExpressoHouse():
                 </html>"""
 
     @expose()
-    def addCustomer(self, Name, Adress, Phone, Username, Password, PaymentType, MembershipClass):
+    def addCustomer(self,personName, adress, phone, username, password, paymentTypeid, membershipid):
+        customer = """INSERT INTO Customer (Name, Adress, Phone, Username, Password, PaymentTypeid, Membershipid) VALUES ("""+personName+""", "5006, Bergen", 238923892, "drew.barrymore", "123456",1,1)"""
+        print(customer)
+        databaseInsert(customer)
+        pass
         return "Customer added"
 
     @expose()
     def showCustomers(self):
+#        showCustomer = """SELECT c.Name, c.Adress, c.Phone, c.Username, p.NameOfPayment, m.MembershipName from Customer c JOIN PaymentType p ON c.MembershipID = m.Membershipid JOIN Membership m  ON c.PaymentTypeID=p.PaymentTypeid"""
+        showCustomer = """SELECT c.Name, c.Adress, c.Phone, c.Username from Customer c """
+        print(showCustomer)
+        showCust = databaseExtract(showCustomer)
+        table = ""
+        for i in range(0, len(showCust)):
+            table = table+ "<tr>"
+            for j in range(0, len(showCust[i])):
+                tableContent = str(showCust[i][j])
+                table = table + "<th>"+tableContent+"</th>"
+            table = table + "</tr>"
+        return """<html>
+                """, head, """
+                  <body>
+                    <div>Welcome to Expresso House</div>
+                    <br /> 
+                     <table style="width:100%">
+                      <tr>
+                        <th>Name</th>
+                        <th>Adress</th>
+                        <th>Phone</th>
+                        <th>Username</th>
+                        <th>Payment Type</th>
+                        <th>Membership Type</th>
+                      </tr>
+                    """+table+"""
+                    </table> 
+
+                                        
+                  </body>
+                </html>"""
         return "Show customers"
 
 
